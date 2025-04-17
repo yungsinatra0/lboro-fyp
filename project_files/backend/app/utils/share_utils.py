f3f9c60a9590aeb16f3552b3fa9392379b66c791
yungsinatra0 <calincor@gmail.com>
2025-04-17 16:34:49 +0100

    need to keep pushing

--- project_files/backend/app/utils/share_utils.py
+++ project_files/backend/app/utils/share_utils.py
@@ -1,11 +1,13 @@
 from sqlmodel import Session, select
 from ..models import Vaccine, Allergy, Medication, HealthData, MedicalHistory, LabResult, AllergyResponse, MedicationResponse, HealthDataResponse, MedicalHistoryResponse, LabTest, LabTestResponse, LabResultResponse, VaccineResponse, MedicalHistoryResponseLab
 
-def get_item_data(grouped_items, session: Session):
+def get_item_data(grouped_items: dict, session: Session):
     items_data = {}
     
     print("-" * 20, "\n")
-    print("Grouped items: ", grouped_items)
+    for type, value in grouped_items.items():
+        print(f"Type: {type}")
+        print(f"Value: {value}")        
     print("\n", "-" * 20)
     
     return items_data       
